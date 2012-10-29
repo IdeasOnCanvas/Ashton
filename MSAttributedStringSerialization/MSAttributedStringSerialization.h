065c89d93e9ff66c4be3b4a5d63dd12799f47b2d
Martin Schürrer <martin@schuerrer.org>
2012-10-29 16:39:32 +0100

    Introduce options for Core Text / App/UIKit

--- MSAttributedStringSerialization/MSAttributedStringSerialization.h
+++ MSAttributedStringSerialization/MSAttributedStringSerialization.h
@@ -1,8 +1,24 @@
 #import <Foundation/Foundation.h>
 
+typedef NS_OPTIONS(NSUInteger, MSHTMLWritingOptions) {
+    // The NSAttributedString contains CoreText attributes
+    MSHTMLWritingCoreTextAttributes = (1UL << 0),
+
+    // The NSAttributedString contains AppKit/UIKit attributes
+    MSHTMLWritingCocoaAttributes = (1UL << 1)
+};
+
+typedef NS_OPTIONS(NSUInteger, MSHTMLReadingOptions) {
+    // Create an NSAttributedString with CoreText attributes
+    MSHTMLReadingCoreTextAttributes = (1UL << 0),
+
+    // Create an NSAttributedString with AppKit/UIKit attributes
+    MSHTMLReadingCocoaAttributes = (1UL << 1)
+};
+
 @interface MSAttributedStringSerialization : NSObject
 
-+ (NSString *)HTMLStringWithAttributedString:(NSAttributedString *)attributedString;
-+ (NSAttributedString *)attributedStringWithHTMLString:(NSString *)htmlString;
++ (NSString *)HTMLStringWithAttributedString:(NSAttributedString *)attributedString options:(MSHTMLWritingOptions)options;
++ (NSAttributedString *)attributedStringWithHTMLString:(NSString *)htmlString options:(MSHTMLReadingOptions)options;
 
 @end
--- MSAttributedStringSerialization/MSAttributedStringSerialization.m
+++ MSAttributedStringSerialization/MSAttributedStringSerialization.m
@@ -3,11 +3,11 @@
 
 @implementation MSAttributedStringSerialization
 
-+ (NSString *)HTMLStringWithAttributedString:(NSAttributedString *)attributedString {
++ (NSString *)HTMLStringWithAttributedString:(NSAttributedString *)attributedString options:(MSHTMLWritingOptions)options {
     return [[[MSASSHTMLWriter alloc] initWithAttributedString:attributedString] HTMLString];
 }
 
-+ (NSAttributedString *)attributedStringWithHTMLString:(NSString *)htmlString {
++ (NSAttributedString *)attributedStringWithHTMLString:(NSString *)htmlString options:(MSHTMLReadingOptions)options {
     return nil;
 }
 
