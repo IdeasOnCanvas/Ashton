19b2db5577b1310ef4bab75f309b8aef44ea641e
Martin Schürrer <martin@schuerrer.org>
2012-10-30 20:18:54 +0100

    Refactoring

--- MSAttributedStringSerialization/MSASSHTMLWriterAppKit.m
+++ MSAttributedStringSerialization/MSASSHTMLWriterAppKit.m
@@ -21,35 +21,26 @@
 }
 
 - (NSDictionary *)stylesForAttribute:(id)attr withName:(NSString *)attrName {
+    NSMutableDictionary *styles = [NSMutableDictionary dictionary];
+
     if ([attrName isEqual:NSParagraphStyleAttributeName]) {
         NSParagraphStyle *paragraphStyle = (NSParagraphStyle *)attr;
 
-        NSString *textAlignment = @"";
-        if ([paragraphStyle alignment] == NSLeftTextAlignment) {
-            textAlignment = @"left";
-        } else if ([paragraphStyle alignment] == NSRightTextAlignment) {
-            textAlignment = @"right";
-        } else if ([paragraphStyle alignment] == NSCenterTextAlignment) {
-            textAlignment = @"center";
-        }
-
-        return @{ @"text-alignment": textAlignment };
+        if ([paragraphStyle alignment] == NSLeftTextAlignment) styles[@"text-align"] = @"left";
+        if ([paragraphStyle alignment] == NSRightTextAlignment) styles[@"text-align"] = @"right";
+        if ([paragraphStyle alignment] == NSCenterTextAlignment) styles[@"text-align"] = @"center";
     }
     if ([attrName isEqual:NSFontAttributeName]) {
         NSFont *font = (NSFont *)attr;
-        NSMutableDictionary *styles = [@{ @"font-family": font.familyName, @"font-size": @(font.pointSize) } mutableCopy];
+        styles[@"font-family"] = font.familyName;
+        styles[@"font-size"] = @(font.pointSize);
 
         NSFontSymbolicTraits symbolicTraits = [[font fontDescriptor] symbolicTraits];
         if ((symbolicTraits & NSFontBoldTrait) == NSFontBoldTrait) styles[@"font-style"] = @"italic";
         if ((symbolicTraits & NSFontItalicTrait) == NSFontItalicTrait) styles[@"font-weight"] = @"bold";
-
-        return styles;
-    }
-    if ([attrName isEqual:NSLinkAttributeName]) {
-        return nil;
     }
     if ([attrName isEqual:NSUnderlineStyleAttributeName]) {
-        NSMutableDictionary *styles = [@{ @"text-decoration": @"underline" } mutableCopy];
+        styles[@"text-decoration"] = @"underline";
 
         if ([attr isEqual:@(NSUnderlineStyleSingle)]) styles[@"-cocoa-underline"] = @"underline";
         if ([attr isEqual:@(NSUnderlineStyleThick)]) styles[@"-cocoa-underline"] = @"thick";
@@ -57,30 +48,28 @@
             styles[@"-cocoa-underline"] = @"double";
             styles[@"text-decoration-style"] = @"double"; // CSS 3 attribute, not yet recognized
         }
-
-        return styles;
     }
     if ([attrName isEqual:NSUnderlineColorAttributeName]) {
-        return @{ @"text-decoration": @"underline", @"text-decoration-color": [self htmlHexString:attr] };
+        styles[@"text-decoration"] =  @"underline";
+        styles[@"text-decoration-color"] = [self htmlHexString:attr];
     }
     if ([attrName isEqual:NSForegroundColorAttributeName] || [attrName isEqual:NSStrokeColorAttributeName]) {
-        return @{ @"color": [self htmlHexString:attr] };
+        styles[@"color"] = [self htmlHexString:attr];
     }
 
     if ([attrName isEqual:NSStrikethroughStyleAttributeName]) {
-        NSMutableDictionary *styles = [@{ @"text-decoration": @"line-through" } mutableCopy];
+        styles[@"text-decoration"] = @"line-through";
 
         if ([attr isEqual:@(NSUnderlineStyleSingle)]) styles[@"-cocoa-strikethrough"] = @"underline";
         if ([attr isEqual:@(NSUnderlineStyleThick)]) styles[@"-cocoa-strikethrough"] = @"thick";
         if ([attr isEqual:@(NSUnderlineStyleDouble)]) styles[@"-cocoa-strikethrough"] = @"double";
-
-        return styles;
     }
     if ([attrName isEqual:NSStrikethroughColorAttributeName]) {
-        return @{ @"text-decoration": @"line-through", @"-cocoa-strikethrough-color": [self htmlHexString:attr] };
+        styles[@"text-decoration"] = @"line-through";
+        styles[@"-cocoa-strikethrough-color"] = [self htmlHexString:attr];
     }
 
-    return nil;
+    return styles;
 }
 
 // From https://github.com/Cocoanetics/DTCoreText/blob/master/Core/Source/DTColor%2BHTML.m
