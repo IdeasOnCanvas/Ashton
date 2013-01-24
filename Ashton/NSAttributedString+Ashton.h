fab14c7db2da015120edf0e292dc880ef41384d8
Martin Schürrer <martin@schuerrer.org>
2013-01-24 11:03:19 +0100

    Make static analyzer happy

--- Ashton/NSAttributedString+Ashton.h
+++ Ashton/NSAttributedString+Ashton.h
@@ -1,11 +1,14 @@
 @interface NSAttributedString (Ashton)
 
+// See http://objcolumnist.com/2011/11/03/keeping-the-static-analyzer-happy-prefixed-initializers/ for an explanation of
+// NS_RETURNS_RETAINED and __attribute__((ns_consumes_self))
+
 // Attributed String with UIKit or AppKit Attributes
 - (NSString *)mn_HTMLRepresentation;
-- (id)mn_initWithHTMLString:(NSString *)htmlString;
+- (id)mn_initWithHTMLString:(NSString *)htmlString NS_RETURNS_RETAINED __attribute__((ns_consumes_self));
 
 // Attributed String with CT Attributes
 - (NSString *)mn_HTMLRepresentationFromCoreTextAttributes;
-- (id)mn_initWithCoreTextAttributesFromHTMLString:(NSString *)htmlString;
+- (id)mn_initWithCoreTextAttributesFromHTMLString:(NSString *)htmlString NS_RETURNS_RETAINED __attribute__((ns_consumes_self));
 
 @end
