bf96cdc2379f7e17beecf610c6d1db94fa4758aa
Martin Schürrer <martin@schuerrer.org>
2013-03-20 16:28:54 +0100

    Indentation

--- OSXExampleTests/AshtonCSSTests.m
+++ OSXExampleTests/AshtonCSSTests.m
@@ -40,7 +40,7 @@
 }
 
 - (void)testMisorderedFontFeaturesAttribute {
-    NSString *styleString = @"-cocoa-font-features: 3/3; font: 14px \"Hoefler Text\"; text-align: left; color: rgba(0, 0, 0, 1.000000); ";
+    NSString *styleString =         @"-cocoa-font-features: 3/3; font: 14px \"Hoefler Text\"; text-align: left; color: rgba(0, 0, 0, 1.000000); ";
     NSString *expectedStyleString = @"color: rgba(0, 0, 0, 1.000000); font: 14px \"Hoefler Text\"; text-align: left; -cocoa-font-features: 3/3; ";
     [self assertStyleStringsEqual:styleString expected:expectedStyleString];
 }
@@ -54,11 +54,11 @@
 - (void)testFutureFontAttribute {
     NSString *styleString, *expectedStyleString;
 
-    styleString = @"font: bold italic 14px \"Helvetica\", \"Arial\", sans-serif; ";
+    styleString =         @"font: bold italic 14px \"Helvetica\", \"Arial\", sans-serif; ";
     expectedStyleString = @"font: bold italic 14px \"Helvetica\"; ";
     [self assertStyleStringsEqual:styleString expected:expectedStyleString];
 
-    styleString = @"color: rgba(0, 0, 0, 0.000000); font: 14px \"Helvetica\", \"Arial\", sans-serif; text-align: left; ";
+    styleString =         @"color: rgba(0, 0, 0, 0.000000); font: 14px \"Helvetica\", \"Arial\", sans-serif; text-align: left; ";
     expectedStyleString = @"color: rgba(0, 0, 0, 0.000000); font: 14px \"Helvetica\"; text-align: left; ";
     [self assertStyleStringsEqual:styleString expected:expectedStyleString];
 }
