72bc356860f5c463c31768ef1a4fbd2498848276
Michael Schwarz <michael_schwarz@me.com>
2017-09-16 10:19:15 +0200

    Initial commit

---
+++ Ashton/Ashton.h
@@ -0,0 +1,19 @@
+//
+//  Ashton.h
+//  Ashton
+//
+//  Created by Michael Schwarz on 16.09.17.
+//  Copyright © 2017 Michael Schwarz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Ashton.
+FOUNDATION_EXPORT double AshtonVersionNumber;
+
+//! Project version string for Ashton.
+FOUNDATION_EXPORT const unsigned char AshtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ashton/PublicHeader.h>
+
+
---
+++ Ashton/Ashton.swift
@@ -0,0 +1,24 @@
+//
+//  Ashton.swift
+//  Ashton
+//
+//  Created by Michael Schwarz on 16.09.17.
+//  Copyright © 2017 Michael Schwarz. All rights reserved.
+//
+
+import Foundation
+
+
+/// transforms AttributedString
+public final class Ashton {
+
+	public typealias HTML = String
+
+	public static func encode(_ attributedString: NSAttributedString) -> HTML {
+		return ""
+	}
+
+	public static func decode(_ html: HTML) -> NSAttributedString {
+		return NSAttributedString()
+	}
+}
