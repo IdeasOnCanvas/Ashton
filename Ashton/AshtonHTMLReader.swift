0e35d0dbeba97f94d8c4b7f2116e9cf31a65eea7
Michael Schwarz <michael_schwarz@me.com>
2017-09-17 17:27:33 +0200

    Add template reader delegate methods

--- Ashton/AshtonHTMLReader.swift
+++ Ashton/AshtonHTMLReader.swift
@@ -11,16 +11,42 @@ import Foundation
 
 final class AshtonHTMLReader: NSObject {
 
+	var output: NSMutableAttributedString!
+
 	func decode(_ html: Ashton.HTML) -> NSAttributedString {
 		guard let data = html.data(using: .utf8) else { return NSAttributedString() }
+		self.output = NSMutableAttributedString()
 
 		let parser = XMLParser(data: data)
 		parser.delegate = self
 
-		return NSAttributedString()
+		return self.output
 	}
 }
 
 extension AshtonHTMLReader: XMLParserDelegate {
 
+	func parserDidStartDocument(_ parser: XMLParser) {
+		self.output.beginEditing()
+	}
+
+	func parserDidEndDocument(_ parser: XMLParser) {
+		self.output.endEditing()
+	}
+
+	func parser(_ parser: XMLParser, foundCharacters string: String) {
+		// Append string content
+	}
+
+	func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+		// add styles element
+	}
+
+	func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+		// remove element styles
+	}
+
+	func parser(_ parser: XMLParser, parseErrorOccurred parseError: Error) {
+		// handle error
+	}
 }
