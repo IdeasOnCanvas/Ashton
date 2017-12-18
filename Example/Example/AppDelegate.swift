710ea9874ea0565349e67b7cd0b1fdc7af382eb5
Michael Schwarz <michael_schwarz@me.com>
2017-12-18 16:48:01 +0100

    Add example project

---
+++ Example/Example/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Example
+//
+//  Created by Michael Schwarz on 18.12.17.
+//  Copyright © 2017 IdeasOnCanvas GmbH. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+}
+
