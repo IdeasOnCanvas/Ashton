97461cc2b13d7084eeb38442b845cf1d96621364
Michael Schwarz <michael_schwarz@me.com>
2018-01-15 13:54:55 +0100

    Add comment regarding mixed content preprocessor

--- Ashton/AshtonObjc/AshtonObjcMixedContentPreprocessor.h
+++ Ashton/AshtonObjc/AshtonObjcMixedContentPreprocessor.h
@@ -11,6 +11,9 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/// TBXML parser cannot handle html mixed content, therefore we have to preprocess it
+/// mixedContent = "<html>test <strong>sample</strong></html>", will be processed to
+/// "<html><wrapped>test </wrapped><strong><wrapped>sample</wrapped></strong></html>
 @interface AshtonObjcMixedContentPreprocessor: NSObject
 
 - (NSString *)preprocessHTMLString:(NSString *)htmlString;
