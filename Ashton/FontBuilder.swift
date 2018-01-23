762fe673cf2b635193ff783f5fd6dd1eabd7bb35
Michael Schwarz <michael_schwarz@me.com>
2018-01-23 18:07:30 +0100

    Improve font cache

--- Ashton/FontBuilder.swift
+++ Ashton/FontBuilder.swift
@@ -11,22 +11,32 @@ import CoreGraphics
 import CoreText
 
 
+/// Creates a NS/UIFont
 struct FontBuilder {
+    var fontName: String? { return self.postScriptName ?? self.familyName }
     var familyName: String?
     var postScriptName: String?
     var isBold: Bool = false
     var isItalic: Bool = false
     var pointSize: CGFloat?
     var fontFeatures: [[String: Any]]?
-    
-    static let fontCache = NSCache<NSString, Font>()
+    var cacheKey: String {
+        guard let familyName = self.fontName else { return "" }
+        guard let pointSize = self.pointSize else { return "" }
+
+        return "\(familyName)\(pointSize)\(self.isBold)\(self.isItalic)\(self.fontFeatures?.description ?? "")"
+    }
+
+    static private(set) var fontCache: [String: Font] = [:]
     
     func makeFont() -> Font? {
-        guard let fontName = self.postScriptName ?? self.familyName else { return nil }
+        guard let fontName = self.fontName else { return nil }
         guard let pointSize = self.pointSize else { return nil }
-        
-        let cacheKey = "\(fontName)\(pointSize)\(self.isItalic)\(self.isBold)"
-        if let cachedFont = FontBuilder.fontCache.object(forKey: cacheKey as NSString) { return cachedFont }
+
+        let cacheKey = self.cacheKey
+        if let cachedFont = FontBuilder.fontCache[cacheKey] {
+            return cachedFont
+        }
 
         var attributes: [FontDescriptor.AttributeName: Any] = [FontDescriptor.AttributeName.name: fontName]
         if let fontFeatures = fontFeatures {
@@ -49,7 +59,7 @@ struct FontBuilder {
 
         let font = CTFontCreateWithFontDescriptor(fontDescriptor, pointSize, nil) as Font
         
-        FontBuilder.fontCache.setObject(font, forKey: cacheKey as NSString)
+        FontBuilder.fontCache[cacheKey] = font
         return font
     }
 }
