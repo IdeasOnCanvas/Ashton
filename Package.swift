63b706d4bffd5e95f9308b8765372a57644035d0
Michael Schwarz <michael_schwarz@me.com>
2020-11-12 22:47:39 +0100

    Add package.swift file

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "Ashton",
+    platforms: [.iOS("13.4"), .macOS(.v10_15)],
+    products: [
+        .library(
+            name: "Ashton",
+            targets: ["Ashton"]),
+    ],
+    targets: [
+        .target(
+            name: "Ashton",
+            dependencies: [],
+            path: "Sources"),
+        .testTarget(
+            name: "AshtonTests",
+            dependencies: ["Ashton"],
+            path: "Tests",
+            exclude: ["TestFiles", "AshtonBenchmark", "Bridging.h", "AshtonBenchmarkTests.swift"]),
+    ]
+)
---
+++ Tests/AshtonBenchmarkTests.swift
@@ -0,0 +1,9 @@
+//
+//  AshtonBenchmarkTests.swift
+//  AshtonTests
+//
+//  Created by Michael Schwarz on 12.11.20.
+//  Copyright © 2020 Michael Schwarz. All rights reserved.
+//
+
+import Foundation
