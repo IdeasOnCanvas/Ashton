62d46ab6e103fbcbc99125632a1f0cb5ced5795d
Marco Masser <marco@ideasoncanvas.com>
2023-08-31 11:07:20 +0200

    Revert changes to Package.swift and add Package@swift-5.9.swift

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -1,9 +1,9 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "Ashton",
-    platforms: [.iOS("13.4"), .macOS(.v10_15)],
+    platforms: [.iOS("13.4"), .macOS(.v10_15), .visionOS(.v1)],
     products: [.library(name: "Ashton", targets: ["Ashton"])],
     targets: [
         .target(
